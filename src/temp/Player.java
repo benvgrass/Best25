0fbdc62010d08b46806c4552658ce57545aefe4b
Ben Grass <coerpixcoerp@gmail.com>
2017-02-02 18:59:45 -0500

    implemented position parse, encapsulated better

--- src/temp/Player.java
+++ src/temp/Player.java
@@ -3,17 +3,69 @@ package temp;
 public class Player {
 	private static int count = 0;
 	
-	public int id;
-	public Position position;
-	public int season;
-	public String name;
-	public double WAR;
+	private int id;
+	private Position position;
+	private int season;
+	private String name;
+	private double WAR;
 	
 	public Player(String position, int season, String name, double WAR) {
 		this.id = ++count;
-//		this.position = position;
+		this.position = parsePosition(position);
 		this.season = season;
 		this.name = name;
 		this.WAR = WAR;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public int getSeason() {
+		return season;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getWAR() {
+		return WAR;
+	}
+
+	private static Position parsePosition(String posString) {
+		switch (posString){
+			case "C":
+				return Position.C;
+			case "B1":
+				return Position.B1;
+			case "B2":
+				return Position.B2;
+			case "B3":
+				return Position.B3;
+			case "SS":
+				return Position.SS;
+			case "LF":
+				return Position.LF;
+			case "CF":
+				return Position.CF;
+			case "RF":
+				return Position.RF;
+			case "OF":
+				return Position.OF;
+			case "DH":
+				return Position.DH;
+			case "SP":
+				return Position.SP;
+			case "RP":
+				return Position.RP;
+			default:
+				System.err.println("Parse error: got default val");
+				return Position.RP;
+		}
 	}
 }
