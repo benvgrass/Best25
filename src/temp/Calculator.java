3174b6dc33c5ba582a618165c39657af2c0079d3
Michael Shulman <Michael Shulman>
2017-02-02 20:05:27 -0500

    moved file pull to a function

--- src/temp/Calculator.java
+++ src/temp/Calculator.java
@@ -1,19 +1,37 @@
 package temp;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Calculator {
 	private static final String dataPath = "data" + File.separator + "jays.csv";
 	
 	public static void main(String[] args) throws IOException {
+		ArrayList<Player> players = getPlayersFromFile();
+		System.out.println(players.size());
+		
+	}
+	
+	private static ArrayList<Player> getPlayersFromFile() throws IOException {
 		File f = new File(dataPath);
 		BufferedReader scanner = new BufferedReader(new FileReader(f));
-		String total = "";
-		String data;
+		
+		ArrayList<Player> players = new ArrayList<Player>();
+		String[] info;
+		Player p;
+		
+		String data = scanner.readLine();
 		while((data = scanner.readLine()) != null) {
-			total += data;
+			 info = data.split(",");
+			 if(info.length != 5) {
+				 System.err.println("info was of lenth " + info.length + "at player " + players.size());
+				 System.exit(-1);
+			 }
+			 p = new Player(info[0], Integer.parseInt(info[1]), info[2], Double.parseDouble(info[3]));
+			 players.add(p);
 		}
-		System.out.print(total.length());
 		scanner.close();
+		
+		return players;
 	}
 }
