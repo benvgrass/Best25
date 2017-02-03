fd847b3ad4bae953727a5fe6e216a72eb144e9d2
Michael Shulman <Michael Shulman>
2017-02-03 16:39:09 -0500

    inicial set up for best rostors

--- src/temp/Calculator.java
+++ src/temp/Calculator.java
@@ -2,14 +2,54 @@ package temp;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Calculator {
 	private static final String dataPath = "data" + File.separator + "jays.csv";
 	
 	public static void main(String[] args) throws IOException {
 		ArrayList<Player> players = getPlayersFromFile();
-		System.out.println(players.size());
+		players.sort(new Comparator<Player>() {
+			@Override
+			public int compare(Player o1, Player o2) {
+				if(o1.getWAR() < o2.getWAR())
+					return 1;
+				else if(o1.getWAR() > o2.getWAR())
+					return -1;
+				return 0;
+			}
+		});
 		
+		// create a new list of possible players to pick from every year
+		ArrayList<ArrayList<Player>> playerYears = new ArrayList<ArrayList<Player>>(25);
+		for(int i = 0; i < 25; i++) {
+			playerYears.add(new ArrayList<Player>());
+		}
+		
+		//add top 25 players per year
+		for(Player p: players) {
+			playerYears.get(p.getSeason() - 1992).add(p);
+		}
+		
+		ArrayList<Rostor> topRostors = new ArrayList<Rostor>(25);
+		for(int i = 0; i < 25; i++) {
+			Rostor r = new Rostor();
+			
+			ArrayList<Player> YearOfPlayers = playerYears.get(i);
+			Position[] totalPositions = {Position.B1, Position.B2, Position.B3, Position.C, Position.C, Position.CF, Position.DH, Position.LF, Position.OF, Position.OF, Position.RF, Position.RP, Position.RP, Position.RP, Position.RP, Position.RP, Position.RP, Position.RP, Position.SP, Position.SP, Position.SP, Position.SP, Position.SP, Position.SS, Position.SS};
+			for(Player p: YearOfPlayers) {
+				if(totalPositions.length == 0)
+					break;
+				
+				if(contains(totalPositions, p.getPosition())) {
+					totalPositions = remove(totalPositions, p.getPosition());
+					r.add(p);
+				}
+			}
+			topRostors.add(r);
+		}
+		
+		//here we will have the best rostors in topRostors
 	}
 	
 	private static ArrayList<Player> getPlayersFromFile() throws IOException {
@@ -33,5 +73,13 @@ public class Calculator {
 		scanner.close();
 		
 		return players;
+	}
+	
+	public static boolean contains(Position[] positions, Position p) {
+		return true; //TODO implement this
+	}
+	
+	public static Position[] remove(Position[] positions, Position p) {
+		return null; //TODO implement this
 	}
 }
---
+++ src/temp/Rostor.java
@@ -0,0 +1,18 @@
+package temp;
+
+import java.util.HashMap;
+
+public class Rostor {
+	
+	//some data kind of bullshit
+	
+	public int getWar() {
+		//TODO return total war
+		return -1;
+	}
+	
+	public void add(Player p) {
+		//TODO implement this
+	}
+
+}
