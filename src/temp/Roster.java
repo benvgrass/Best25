fa8039bd18a1df22d358a5f9ff96fdc165deae4a
Ben Grass <coerpixcoerp@gmail.com>
2017-02-09 17:17:03 -0500

    changed Roster to reflect current algorithm, added RosterPosition to serve as a position on a roster

--- src/temp/Roster.java
+++ src/temp/Roster.java
@@ -1,45 +1,58 @@
 package temp;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Roster {
 	
-	List<Player> players;
-	HashMap<Position, Integer> available;
+	private HashMap<Position, RosterPosition> available;
+	private HashMap<Integer, Integer> playerIDs;
 
 	public Roster () {
-		players = new ArrayList<>();
 		available = new HashMap<>();
-		available.put(Position.C, 2);
-		available.put(Position.B1, 1);
-		available.put(Position.B2, 1);
-		available.put(Position.B3, 1);
-		available.put(Position.SS, 2);
-		available.put(Position.LF, 1);
-		available.put(Position.CF, 1);
-		available.put(Position.RF, 1);
-		available.put(Position.OF, 2);
-		available.put(Position.SP, 5);
-		available.put(Position.RP, 7);
-		available.put(Position.DH, 1);
+		available.put(Position.C, new RosterPosition(2));
+		available.put(Position.B1, new RosterPosition(1));
+		available.put(Position.B2, new RosterPosition(1));
+		available.put(Position.B3, new RosterPosition(1));
+		available.put(Position.SS, new RosterPosition(2));
+		available.put(Position.LF, new RosterPosition(1));
+		available.put(Position.CF, new RosterPosition(1));
+		available.put(Position.RF, new RosterPosition(1));
+		available.put(Position.OF, new RosterPosition(2));
+		available.put(Position.SP, new RosterPosition(5));
+		available.put(Position.RP, new RosterPosition(7));
+		available.put(Position.DH, new RosterPosition(1));
+		playerIDs = new HashMap<>();
 	}
 	
 	public double getWar() {
 		int sum = 0;
-		for (Player p: players) sum += p.getWAR();
+		for (RosterPosition r: available.values()) sum += r.getWAR();
 		return sum;
 	}
 	
 	public boolean add(Player p) {
-		int availableNumber;
-		if ((availableNumber = available.get(p.getPosition())) > 0) {
-			players.add(p);
-			available.replace(p.getPosition(), availableNumber - 1);
-		}
 
-		return false;
+		if (playerIDs.containsKey(p.getfID())) return false;
+		RosterPosition r = available.get(p.getPosition());
+		if (!r.addPlayer(p)) return false;
+
+		available.put(p.getPosition(), r);
+		playerIDs.put(p.getfID(), p.getfID());
+		return true;
+	}
+
+	public LinkedList<Player> getPlayers() {
+		LinkedList<Player> players = new LinkedList<>();
+		for (RosterPosition r: available.values()) players.addAll(r.getPlayers());
+		return players;
+	}
+
+	public boolean removePlayer(Player p) {
+		playerIDs.remove(p.getfID());
+		RosterPosition r = available.get(p.getPosition());
+		if (!r.removePlayer(p)) return false;
+		available.put(p.getPosition(), r);
+		return true;
 	}
 
 }
---
+++ src/temp/RosterPosition.java
@@ -0,0 +1,42 @@
+package temp;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Serves as Position on a roster
+ */
+public class RosterPosition {
+    private final int max;
+    private int numPlayers;
+    private LinkedList<Player> players;
+    public RosterPosition(int max) {
+        this.max = max;
+        this.players = new LinkedList<>();
+        this.numPlayers = 0;
+    }
+
+    public double getWAR() {
+        double sum = 0;
+        for (Player player : players) sum += player.getWAR();
+        return sum;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public boolean addPlayer(Player player) {
+        if (numPlayers < max) {
+            players.add(player);
+            numPlayers++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removePlayer(Player p) {
+        return players.remove(p);
+    }
+
+}
