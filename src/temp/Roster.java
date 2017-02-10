3a434def00fd9f6b92ac7bc063fc4a14195158da
Ben Grass <coerpixcoerp@gmail.com>
2017-02-09 22:30:41 -0500

    Re-implemented Roster based on n-queens implementation.

--- src/temp/Roster.java
+++ src/temp/Roster.java
@@ -4,45 +4,24 @@ import java.util.*;
 
 public class Roster {
 	
-	private HashMap<Position, RosterPosition> available;
-	private HashMap<Integer, Integer> playerIDs;
+	private HashMap<Integer, Player> players;
 
 	public Roster () {
-		available = new HashMap<>();
-		available.put(Position.C, new RosterPosition(2));
-		available.put(Position.B1, new RosterPosition(1));
-		available.put(Position.B2, new RosterPosition(1));
-		available.put(Position.B3, new RosterPosition(1));
-		available.put(Position.SS, new RosterPosition(2));
-		available.put(Position.LF, new RosterPosition(1));
-		available.put(Position.CF, new RosterPosition(1));
-		available.put(Position.RF, new RosterPosition(1));
-		available.put(Position.OF, new RosterPosition(2));
-		available.put(Position.SP, new RosterPosition(5));
-		available.put(Position.RP, new RosterPosition(7));
-		available.put(Position.DH, new RosterPosition(1));
-		playerIDs = new HashMap<>();
+		players = new HashMap<>();
 	}
 
 	public double getWar() {
-		int sum = 0;
-		for (RosterPosition r: available.values()) sum += r.getWAR();
+		double sum = 0;
+		for (Player p: players.values()) sum += p.getWAR();
 		return sum;
 	}
 	
 	public boolean add(Player p) {
-
-		if (playerIDs.containsKey(p.getfID())) return false;
-		RosterPosition r = available.get(p.getPosition());
-		if (!r.addPlayer(p)) return false;
-
-		available.put(p.getPosition(), r);
-		playerIDs.put(p.getfID(), p.getfID());
-		return true;
+		return players.putIfAbsent(p.getfID(), p) == null;
 	}
 
-	public LinkedList<Player> getPlayers() {
-		LinkedList<Player> players = new LinkedList<Player>() {
+	public Collection<Player> getPlayers() {
+		Collection<Player> players = new TreeSet<Player>(Comparator.comparing(Player::getSeason)) {
 			@Override
 			public String toString() {
 				Iterator<Player> it = iterator();
@@ -60,49 +39,17 @@ public class Roster {
 			}
 
 		};
-
-		for (RosterPosition r: available.values()) players.addAll(r.getPlayers());
+		players.addAll(this.players.values());
 		return players;
 	}
 
 	public boolean removePlayer(Player p) {
-		playerIDs.remove(p.getfID());
-		RosterPosition r = available.get(p.getPosition());
-		if (!r.removePlayer(p)) return false;
-		available.put(p.getPosition(), r);
-		return true;
-	}
-
-	public Set<Position> availablePositions() {
-		Set<Position> positions = new HashSet<>();
-		for (Position p: Position.values()) {
-			if (available.get(p).open()) {
-				positions.add(p);
-			}
-		}
-		return positions;
+		return players.remove(p.getfID(), p);
 	}
 
 	@Override
 	public String toString() {
-		return "Roster{" +
-				"available=" + available +
-				'}';
+		return getPlayers().toString();
 	}
 
-	public HashMap<Position, RosterPosition> getAvailable() {
-		return available;
-	}
-
-	public HashMap<Integer, Integer> getPlayerIDs() {
-		return playerIDs;
-	}
-
-	public int remainingSpots(Position p) {
-		return available.get(p).remainingSpots();
-	}
-
-	public static void main(String[] args) {
-
-	}
 }
