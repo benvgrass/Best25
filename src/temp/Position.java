8e3c278d27b9325b5919c4652edb0255ad8fd073
Ben Grass <coerpixcoerp@gmail.com>
2017-03-23 18:19:26 -0400

    added maxPlayersForPosition to compare w/ BP

--- src/temp/Position.java
+++ src/temp/Position.java
@@ -6,5 +6,6 @@ public enum Position {
 
     // maxPlayerForPosition[Position] is the max number of players that can be at that position
     public static final int[] maxPlayersForPosition =  {5,2,1,1,1,2,1,1,1,7,2,1};
+//    public static final int[] maxPlayersForPosition =  {5,2,2,2,1,2,1,1,1,6,1,1};
 
 }
